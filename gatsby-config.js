5a0f51812f122072807e6f28db54434eaf081bb4
Arnas Kromelis <arnas.kromelis@gmail.com>
2021-02-05 13:22:12 +0100

    feat: DatoCMS integration

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,33 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-source-datocms`,
+      options: {
+        // You can find your read-only API token under the Settings > API tokens
+        // section of your administrative area. Make sure to grant both CDA and CMA permissions.
+        apiToken: process.env.GATSBY_API_TOKEN,
+
+        // The project environment to read from. Defaults to the primary environment:
+        environment: `main`,
+
+        // If you are working on development/staging environment, you might want to
+        // preview the latest version of records instead of the published one:
+        previewMode: process.env.NODE_ENV === "development",
+
+        // Disable automatic reloading of content when some change occurs on DatoCMS:
+        disableLiveReload: false,
+
+        // Custom API base URL (most don't need this)
+        // apiUrl: 'https://site-api.datocms.com',
+
+        // Setup locale fallbacks
+        // In this example, if some field value is missing in Italian, fall back to English
+        localeFallbacks: {
+          it: ["en"],
+        },
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,53 @@
 import React from "react"
-import { Link } from "gatsby"
-
+import { useStaticQuery, graphql } from "gatsby"
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
-)
+const IndexPage = () => {
+  const { allDatoCmsArticle } = useStaticQuery(graphql`
+    query SiteTitleQuery {
+      allDatoCmsArticle {
+        nodes {
+          content
+          duration
+          id
+          locale
+          meta {
+            createdAt
+            firstPublishedAt
+            updatedAt
+            status
+            publishedAt
+            isValid
+          }
+          model {
+            id
+          }
+          title
+          testField
+        }
+      }
+    }
+  `)
+
+  console.log(allDatoCmsArticle)
+
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <h1>Articles ({allDatoCmsArticle.nodes.length})</h1>
+      <div className="articles">
+        {allDatoCmsArticle.nodes.map(article => (
+          <article key={article.id}>
+            <h2>{article.title}</h2>
+            <div>Duration: {article.duration}</div>
+            <div>Test field: {article.testField}</div>
+            <p>{article.content}</p>
+          </article>
+        ))}
+      </div>
+    </Layout>
+  )
+}
 
 export default IndexPage
